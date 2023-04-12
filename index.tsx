339a3b9370181d69c16f0585aaff69299ed7a9e1
joshuapaulanderson <126980108+joshuapaulanderson@users.noreply.github.com>
2023-04-12 15:12:09 -0500

    Bootstrap Min CSS

--- index.tsx
+++ index.tsx
@@ -14,9 +14,10 @@ import './src/styles/burger.css'
 import './src/styles/styles.css'
 
 if(useStyles) {
+  document.head.insertAdjacentHTML('beforeend','<meta name="viewport" content="width=device-width, initial-scale=1" />');
   document.head.insertAdjacentHTML('beforeend', '<link rel="preconnect" href="https://rsms.me/"><link rel="stylesheet" href="https://rsms.me/inter/inter.css">');
-  document.head.insertAdjacentHTML('beforeend',
-  `<link rel="stylesheet" href="./index.css" type="text/css" />`
+  document.head.insertAdjacentHTML('beforeend','<link href="./styles/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">');
+  document.head.insertAdjacentHTML('beforeend',`<link rel="stylesheet" href="./index.css" type="text/css" />`
   );
 }
 
