c962c779b28e9c0f4ab4ca557156dac5e136c0be
Joshua Brewster (Bubba Skrimp) <brewster.joshua1@gmail.com>
2023-04-14 18:04:10 -0700

    comment

--- index.tsx
+++ index.tsx
@@ -11,14 +11,13 @@ const useStyles = true;
 import './src/components/lib/src/index.css'
 import './src/styles/index.scss'
 import './src/styles/burger.css'
-import './src/styles/inter.css'
+//import './src/styles/inter.css'
 import './src/styles/styles.css'
-import './src/styles/bootstrap.min.css'
+//import './src/styles/bootstrap.min.css'
 
 if(useStyles) {
   document.head.insertAdjacentHTML('beforeend','<meta name="viewport" content="width=device-width, initial-scale=1" />');
-  document.head.insertAdjacentHTML('beforeend',`<link rel="stylesheet" href="./index.css" type="text/css" />`
-  );
+  document.head.insertAdjacentHTML('beforeend',`<link rel="stylesheet" href="./index.css" type="text/css" />`);
 }
 
 let container = document.createElement('div');
