99134e95534bfcdee53bea813cec0e87956001c9
daniamorris <daniamorris@gmail.com>
2023-05-31 13:35:35 -0700

    header and userbar styling

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -3,6 +3,9 @@ import { sComponent } from '../state.component';
 import Button from 'react-bootstrap/Button';
 import { showNotification } from '../../scripts/alerts'
 import { client, logoutSequence } from '../../scripts/client';
+import Badge from 'react-bootstrap/Badge';
+import * as Icon from 'react-feather';
+
 let myalyceLogo = './assets/myalyce.png';
 let profilePic = './assets/JoshBrew.jpg';
 
@@ -17,7 +20,7 @@ export class Header extends sComponent {
         return(
             <header className="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
                  <a className="col-md-3 col-lg-2 me-0 px-3 fs-6" href="#"><img className="img-fluid" width="120" alt="myAlyce" src={myalyceLogo} /></a>
-                  <Button variant="outline-light" onClick={()=>{showNotification("Hello World","Exmaple Notification")}}>Notifications</Button>{' '}
+                  <Icon.Bell className="align-text-bottom" color="white" size={30}></Icon.Bell>
                  <div className="navbar-nav">
                      <div className="nav-item text-nowrap">
                      {this.state.isLoggedIn ? <a className="nav-link px-3" onClick={()=>{ logoutSequence(); }}>
--- src/components/ui/UserAlerts.tsx
+++ src/components/ui/UserAlerts.tsx
@@ -5,7 +5,7 @@ import Badge from 'react-bootstrap/Badge';
 
 export function UserAlerts(props:{streamId?:string}) {
   return (
-    <Card style={{ width: '5rem' }}>
+    <Card>
       <Card.Body>
       <Icon.Bell className="align-text-bottom" size={40}></Icon.Bell>
       <Badge bg="danger">3</Badge>
