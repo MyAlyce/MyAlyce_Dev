211167bf3e04dc4975ad94f209181c32b093f846
Joshua Brewster (Bubba Skrimp) <brewster.joshua1@gmail.com>
2023-06-01 19:38:53 -0700

    Update Widget.tsx

--- src/components/widgets/Widget.tsx
+++ src/components/widgets/Widget.tsx
@@ -13,9 +13,9 @@ export class Widget extends Component<{
         return (
             <Card style={ this.props.style }>
                 { this.props.header ? <Card.Header>{this.props.header}</Card.Header> : null}
+                <Card.Body>
                 { this.props.title ? <Card.Title>{this.props.title}</Card.Title> : null}
                 { this.props.subtitle ? <Card.Subtitle >{this.props.subtitle}</Card.Subtitle > : null}
-                <Card.Body>
                 { this.props.content }
                 </Card.Body>
             </Card>
