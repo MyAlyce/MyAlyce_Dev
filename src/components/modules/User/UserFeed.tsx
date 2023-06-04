a84308b79a7ecb02cada395c46bfe3abfa27d7b9
Joshua Brewster (Bubba Skrimp) <brewster.joshua1@gmail.com>
2023-06-03 23:00:39 -0700

    Update UserFeed.tsx

--- src/components/modules/User/UserFeed.tsx
+++ src/components/modules/User/UserFeed.tsx
@@ -9,15 +9,15 @@ export function UserFeed(props:{streamId?:string, width?:string}) {
   return (
       <Col className="my-auto" style={{minWidth:props.width, borderRadius:'10px', backgroundColor:'rgba(255,255,255,0.9)'}}>
         <Row>
-          <Col className="my-auto" >
+          <Col className="my-auto" title="Heart Rate">
             <BeatingSVG subscribeTo={props.streamId ? props.streamId+'hr' : 'hr'} objectKey={'hr'}/>
                 <br/><StreamText movingAverage={3} stateKey={props.streamId ? props.streamId+'hr' : 'hr'} objectKey={'hr'}/> /min
           </Col> 
-          <Col className="my-auto" >
+          <Col className="my-auto" title="Heart Rate Variability">
             <BeatingSVG subscribeTo={props.streamId ? props.streamId+'hr' : 'hr'} objectKey={'hr'} customContent={<Icon.Activity color="darkgreen" size={40}/>}/>
             <br/><br/><StreamText stateKey={props.streamId ? props.streamId+'hr' : 'hr'} objectKey={'hrv'} movingAverage={3} /> HRV
           </Col>
-          <Col className="my-auto" >
+          <Col className="my-auto" title="Breathing Rate">
             <BeatingSVG subscribeTo={props.streamId ? props.streamId+'breath' : 'breath'} bpm={0.00001} objectKey={'breath'} customContent={<Icon.Wind size={40}></Icon.Wind>}/>
             <br/><br/><StreamText stateKey={props.streamId ? props.streamId+'breath' : 'breath'} objectKey={'breath'}/> /min
           </Col>
